ce144344903e69ad3f93c43ec3dd0c7a2bef533f
lilprof01 <adewumime@gmail.com>
2024-06-24 03:52:54 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - entry point prints the input message
  *
- * Return : 1 (success)
+ * Return : Always 0 (success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-		return (1);
+	char str[] = "and that piece of art is useful\" - Dora korpar, 2015-1019\n";
+
+	write(1, str, strlen(str));
+	return (1);
 }
