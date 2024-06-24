8b3914e60da17994783e69b8b2d8d2e05fda5218
lilprof01 <adewumime@gmail.com>
2024-06-23 19:57:34 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point prints the message
+ * with proper grammar, but the outcome is a piece of art,
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		Return (0);
+}
