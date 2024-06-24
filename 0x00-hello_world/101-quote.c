3534ca784897e1f770fae090cacfa9454c65914a
lilprof01 <adewumime@gmail.com>
2024-06-23 21:32:53 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point prints the input message
+ *
+ * Return : 1 (success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return (1);
+}
