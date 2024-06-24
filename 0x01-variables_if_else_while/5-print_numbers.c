dde90e3a5afab62f5d1c2d377832f10cf84da642
lilprof01 <adewumime@gmail.com>
2024-06-24 11:32:25 -0700

    done

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+*main - entry point prints 0 to 9
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char a;
 
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
