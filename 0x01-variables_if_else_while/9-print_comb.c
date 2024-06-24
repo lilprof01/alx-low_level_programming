9a9eec114d51f0c4bc1365a974455f4fddd7e9f8
lilprof01 <adewumime@gmail.com>
2024-06-24 15:26:13 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entry point prints all combinations of single digit numbers
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	if (x < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	return(0);
+}
