eeede382854cb2f31ca34512f60fc6ea01135dbc
lilprof01 <adewumime@gmail.com>
2024-06-23 19:44:09 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - entry point prints Programming is like building a multilingual puzzle.
  *
- * Return: Always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 		return (0);
 }
