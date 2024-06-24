2449548e2c611c5fde156657e6d9dd04b164469d
lilprof01 <adewumime@gmail.com>
2024-06-24 15:53:40 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - entry point prints all possible combinations of two digits
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 8; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+
+			if (! (x == 8 && y == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
