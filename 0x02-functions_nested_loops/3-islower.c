6f407bf9b1799a9d42930590d66133c37a12bf75
lilprof01 <adewumime@gmail.com>
2024-06-25 16:55:48 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
 *_islower - entry point checks for lowercase letters
-*c is the character to be checked
+*c: is the character to be checked
 *Return: 1 if lowercase character or 0 if not
 */
 int _islower(int c)
