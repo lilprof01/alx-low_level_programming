5d491e0d5015cb37391445abd919ca639cbc6bd9
lilprof01 <adewumime@gmail.com>
2024-06-25 16:39:19 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - entry point prints alphabet 10times
+*
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+	i = 0;	
+
+	while (i < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
