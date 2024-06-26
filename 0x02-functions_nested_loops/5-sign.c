5f6a29b1bf68eb3561e2ff458adadc9f6b89c020
lilprof01 <adewumime@gmail.com>
2024-06-25 17:21:13 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_sign - entry point prints sign of a number
+*
+*Return: 1 if greater than or 0 if it is zero or -1 if less than
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
