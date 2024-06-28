46c6d30bf81e6bc82b3ebc53ccfa9fe9ffd0aca1
lilprof01 <adewumime@gmail.com>
2024-06-28 05:17:53 -0700

    multiplier

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,9 @@
 *mul - entry point multiplies two integer
 *@a: is the first integer
 *@b: is the second integer
-*@c: is the result of the multiplicaion
 *Return: result of multiplication
 */
 int mul(int a, int b)
 {
-	int c;
-
-	c = a * b;
-
-	return (c);
+	return (a * b);
 }
