c8bb65fe82230202f8171b5bfc5a8ab6af445244
lilprof01 <adewumime@gmail.com>
2024-06-28 09:12:09 -0700

    diagonal line printer

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_diagonal - entry point prints a diagonal line in the terminal
+*
+*/
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+	for (j = 1; j < i; j++)
+	{
+		_putchar(' ');
+	}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
