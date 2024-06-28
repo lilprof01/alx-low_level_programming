4203729e689ca54ff1cd6d6e96b17355a0139b44
lilprof01 <adewumime@gmail.com>
2024-06-28 05:49:22 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,14 @@
 
 /**
 *print_numbers - entry point prints all numbers from 0 through to 9
-*@c: charcater to be computed
-*Return: void
 */
 void print_numbers(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c);
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
