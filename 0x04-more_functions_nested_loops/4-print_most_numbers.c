ebd2ec59d40b23dc8c9e08fa5e7af921018fc4fe
lilprof01 <adewumime@gmail.com>
2024-06-28 05:56:31 -0700

    print all numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_most_numbers - entry point prints most numbers asides 2 and 4
+*/
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+		{
+			_putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
