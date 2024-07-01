53e2a3273b249c192a3048807ef184a37fedb430
lilprof01 <adewumime@gmail.com>
2024-07-01 13:23:49 -0700

    wrote a program tkes the value of a pointer as a parameter and updates the value its pointing to

--- 0x05--pointers_arrays_strings/0-reset_to_98.c
+++ 0x05--pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
 *reset_to_98 - entry point takes a pointer as a parameter
 *and updates the value it points to to 98
+*@*n: is the pointer to the integer
 *Return: void
 */
 void reset_to_98(int *n)
