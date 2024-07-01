132dd95476f9e73a9dba9caf21eddf2c457fa49d
lilprof01 <adewumime@gmail.com>
2024-07-01 14:25:09 -0700

    wrote a program that prints the length of a given string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_strlen - entry point prints the length of a string
+*@s: is the pointer to the string length
+*Return: length odf string
+*/
+int _strlen(char *s)
+{
+	return (*s);
+}
