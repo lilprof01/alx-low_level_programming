27faea1cb6898aacb1bb3d3054b02656721e1d6e
lilprof01 <adewumime@gmail.com>
2024-07-01 13:41:21 -0700

    wrote a program that swaps the value of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*swap_int - entry point swaps the value of two integers
+*@a: is the first integer
+*@b: is teh second integer
+*Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
