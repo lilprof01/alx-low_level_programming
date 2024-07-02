f8e494a98d0ca643702fa0ab20157d2f6679a657
lilprof01 <adewumime@gmail.com>
2024-07-01 20:44:37 -0700

    wrote a program that returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <string.h>
 
 /**
 *_strlen - entry point prints the length of a string
 *@s: is the pointer to the string length
-*@c: is the value of string length
 *Return: length odf string
 */
 int _strlen(char *s)
 {
-	int c = *s;
-
-	return (c);
+	
+	{
+		return (strlen(s));
+	}
 }
