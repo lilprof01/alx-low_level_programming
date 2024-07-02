8bacc2f2195e32c2201e569da36a65a146054a43
lilprof01 <adewumime@gmail.com>
2024-07-01 21:31:34 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_rev - entry point prints a string in reverse
+*@s: is the string
+*Return: void
+*/
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
