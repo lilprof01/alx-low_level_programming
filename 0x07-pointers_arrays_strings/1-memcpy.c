f608da36486da2834c53c85875c6bb1acd5e8752
lilprof01 <adewumime@gmail.com>
2024-07-08 15:44:04 -0700

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,11 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *srcptr = (char *)src;
-	char *destptr = (char *)dest;
 	unsigned int i;
 
 	for (i = 0; i <= n; i++)
 	{
-		*destptr = *srcptr;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
