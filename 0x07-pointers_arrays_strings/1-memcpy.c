e2f36acaffd05a5c8117e8427608b3a8bcf59ea6
lilprof01 <adewumime@gmail.com>
2024-07-08 15:05:08 -0700

    fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,18 +3,20 @@
 
 /**
 * *_memcpy - entry point copies n bytes of a memory area to another
-*@dest: ids the destination memory
+*@dest: is the destination memory
 *@src: is the memory source to copy from
 *@n: is the number of bytes to be copied
 *Return: dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *ptr = (char *)src;
+	char *srcptr = (char *)src;
+	char *destptr = (char *)dest;
+	unsigned int i;
 
-	while (n--)
+	for (i = 0; i <= n; i++)
 	{
-		*ptr++ = *dest;
+		*destptr = *srcptr;
 	}
 	return (dest);
 }
