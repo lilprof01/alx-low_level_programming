0f6326d471292bbdd24109106188932fe7fe2d72
lilprof01 <adewumime@gmail.com>
2024-07-08 12:28:13 -0700

    wrote a program that copies specified bytes of memory from one memory area to another

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_memcpy - entry point copies n bytes of a memory area to another
+*@dest: ids the destination memory
+*@src: is the memory source to copy from
+*@n: is the number of bytes to be copied
+*Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = (char *)src;
+
+	while (n--)
+	{
+		*ptr = *dest;
+	}
+	return (dest);
+}
