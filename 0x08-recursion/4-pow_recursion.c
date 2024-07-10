adf622721a96d96df03672bbb25abf9f84e5e473
lilprof01 <adewumime@gmail.com>
2024-07-10 16:08:13 -0700

    wrote a program that returns the value of x raised to y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,3 +1,31 @@
 #include "main.h"
 
-
+/**
+*_pow_recursion - entry point returns value of x raised to y
+*@x: is the base value
+*@y: is the power value
+*Return: value of x raised to y if success or 0 if not
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
