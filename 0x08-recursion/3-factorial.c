97e74a54de60a9d9972b9d3de9b35abe75ca1f5a
lilprof01 <adewumime@gmail.com>
2024-07-10 15:57:06 -0700

    wrote a program that finds the factorial of a given value

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
