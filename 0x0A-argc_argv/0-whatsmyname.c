db0653a7af977b462728a751cace296f6af3b256
lilprof01 <adewumime@gmail.com>
2024-07-12 13:33:30 -0700

    wrote a program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*main - prints out the name of program that calls it
+*@argc: is the arguments count
+*@argv: is the arguments strings
+*Return: always 0 (successful)
+*/
+int main(int argc, char* argv[])
+{
+	(void) argc;
+
+	printf("%s\n" , argv[0]);
+	return (0);
+}
