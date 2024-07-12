f5f657c7430bb01dfae959286dae8399eeb2cb6c
lilprof01 <adewumime@gmail.com>
2024-07-12 13:36:03 -0700

    wrote a program that prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,10 @@
 *@argv: is the arguments strings
 *Return: always 0 (successful)
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	printf("%s\n" , argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
