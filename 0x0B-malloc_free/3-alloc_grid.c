9abef6167a1970bfa68d6eca9f5bcc72a87fb7d3
lilprof01 <adewumime@gmail.com>
2024-07-16 09:04:28 -0700

    wrote a program that allocates memory for multidemensinal arrays

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*
+*
+*
+*
+*/
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	a = (int **)malloc(height * sizeof(int *));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		a[i] = (int *)malloc(width * sizeof(int));
+	if (a[i] == NULL)
+	{
+		for (j = 0; j < i; j++)
+	{
+		free(a[j]);
+        }
+		free(a);
+		return (NULL);
+	}
+	for (j = 0; j < width; j++)
+	{
+		a[i][j] = 0;
+	}
+	}
+	return (a);
+}
