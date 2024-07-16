c9e4a4c3c886b75e52e62022947c4bcd9d20df96
lilprof01 <adewumime@gmail.com>
2024-07-16 16:48:25 -0700

    wrote a program that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,25 +13,21 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2, len;
 	char *a;
 
-	if (s1 == NULL)
+	if (s1 != NULL)
 	{
-		s1 = "";
+		len1 = 0;
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
 	}
-	if (s2 == NULL)
+	if (s2 != NULL)
 	{
-		s2 = "";
-	}
-	i = len1 = len2 = 0;
-	while (s1[i] != '\0')
-	{
-		i++;
-		len1++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		i++;
-		len2++;
+		len2 = 0;
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
 	}
 	len = len1 + len2;
 	a = (char *)malloc((len + 1) * sizeof(char));
