89adbe54e9d5958c02d98479fff2476dd678be20
lilprof01 <adewumime@gmail.com>
2024-07-16 03:41:34 -0700

    created a program that creates an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+**create_array - creates an array of characters
+*@size: is the size of the array
+*@c: is the charcater to be intialized and printed
+*Return: null if size is 0 or a is null, or a if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(size * sizeof(a));
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+	printf("\n");
+	return (a);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+readme file or memory allocation project
