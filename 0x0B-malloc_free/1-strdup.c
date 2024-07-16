c397c4a184364753050e30d82279f44fa27c771a
lilprof01 <adewumime@gmail.com>
2024-07-16 16:00:34 -0700

    wrote a program that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,14 +10,18 @@
 */
 char *_strdup(char *str)
 {
-	int len;
+	int i, len;
 	char *a;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	len = strlen(str);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 	a = (char *)malloc((len + 1) * sizeof(char));
 
 	if (a == NULL)
@@ -26,7 +30,11 @@ char *_strdup(char *str)
 	}
 	else
 	{
-		strcpy(a, str);
+		for (i = 0; i < len; i++)
+		{
+			a[i] = str[i];
+			a[len] = '\0';
+		}
 		return (a);
 	}
 }
