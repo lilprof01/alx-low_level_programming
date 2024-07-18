f7c884961d2af6f9b1277db32ad9c55d7ae6328c
lilprof01 <adewumime@gmail.com>
2024-07-18 14:19:15 -0700

    wrote a program that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 /**
+**malloc_checked - allocates memory
+*@b: is the number of bytes to be allocated
 *
-*
-*
-*
+*Return: Void pointer to the allocated memory
 */
 void *malloc_checked(unsigned int b)
 {
