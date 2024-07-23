2ab6108cc5fbe3ad57de361309f7de3e977acdcf
lilprof01 <adewumime@gmail.com>
2024-07-22 20:41:53 -0700

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,11 +3,11 @@
 
 /**
 *new_dog - creates a new dog
-*@name: name of the dog
-*@age: age of the dog
-*@owner: owner of the dog
+*@name: is the name of the dog
+*@age: is the age of the dog
+*@owner: is the owner of the dog
 *
-*Return: pointer to new dog
+*Return: pointer to the new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
