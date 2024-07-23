3eff5983ee2d34aa30f9fc5fa8987af2b29b7089
lilprof01 <adewumime@gmail.com>
2024-07-22 19:02:40 -0700

    wrote a program that constructs a struct for a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog - is the dog class
+* @name: is the name of the dog
+* @age: is the age of the dog
+* @owner: is the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
